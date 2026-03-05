daa8eb06e87844b11faf902c0b6cd4de6b6a15ac
Rubén Llanes Rioja <rubenllanes47@gmail.com>
2026-03-05 23:28:45 +0100

    Api maps integrada
    Funcionalidad basica de localizacion en tiempo real super basica creada.

--- app-android/app/src/main/java/ifp/tfg_grua_app/MainActivity.kt
+++ app-android/app/src/main/java/ifp/tfg_grua_app/MainActivity.kt
@@ -1,20 +1,45 @@
 package ifp.tfg_grua_app
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import ifp.tfg_grua_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnToMaps.setOnClickListener {
+            ChangeActivity(this, MapsActivity::class.java)
+        }
+    }
+
+    private fun <T> ChangeActivity(context: Context, cls: Class<T>){
+        context.startActivity(Intent(context, cls))
+
+        if(context is Activity){
+            context.finish()
         }
     }
 }
