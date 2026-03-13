2bbde93dcd9f4493e76d4360853ac0a1eb221863
Gus <gabriel.mendez.sas@gmail.com>
2026-03-13 17:40:56 +0100

    admin

---
+++ web-angular/src/app/admin-servicios/admin-servicios.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-servicios',
+  imports: [],
+  templateUrl: './admin-servicios.html',
+  styleUrl: './admin-servicios.css',
+})
+export class AdminServicios {}
--- web-angular/src/app/app.routes.ts
+++ web-angular/src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Home } from './home/home';
+import { AdminPage } from './admin-page/admin-page';
+import { AdminDashboard } from './admin-dashboard/admin-dashboard';
+import { AdminServicios } from './admin-servicios/admin-servicios';
 
 export const routes: Routes = [
+    
+    {
+        path: "admin", component:AdminPage,
+        children:[
+            {path: "admin-dashboard", component:AdminDashboard},
+            {path: "admin-servicios", component:AdminServicios},
+        ]
+    },
     {path: "login", component:Login},
     {path: "home", component:Home},
     {path: '', redirectTo: '/login', pathMatch: 'full'},
