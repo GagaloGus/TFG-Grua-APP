95253f8dcff169c818fb8f172f2577d02947b999
Gus <gabriel.mendez.sas@gmail.com>
2026-03-18 17:50:17 +0100

    dashboard y scss

--- web-angular/src/app/admin-dashboard/admin-dashboard.ts
+++ web-angular/src/app/admin-dashboard/admin-dashboard.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css',
 })
-export class AdminDashboard {}
+export class AdminDashboard {
+  activeServices: number = 0;
+  completedServices: number = 0;
+  availableVehicles: number = 0;
+  availableDrivers: number = 0;
+}
